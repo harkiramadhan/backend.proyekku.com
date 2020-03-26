91815e2ecd888228001d64daebe80f06d39a64f7
harkiramadhan <harkiramadhan@gmail.com>
2020-03-26 21:13:18 +0700

    update

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,7 +6,40 @@ class Login extends CI_Controller{
     $this->load->model('M_Auth');
   }
 
-    function index(){
-      $this->load->view('login');
+  function index(){
+    $this->load->view('login');
+  }
+
+  function auth(){
+    $username = $this->input->post('username', TRUE);
+    $password = $this->input->post('password', TRUE);
+
+    $cek = $this->M_Auth->userLogin($username, $password);
+
+    if($cek->num_rows() > 0){
+      $data = $cek->row();
+
+      if($data->status == "active"){
+        $this->session->set_userdata('masuk', TRUE);
+        $this->session->set_userdata('role', $data->idrole);
+        $this->session->set_userdata('username', $data->username);
+
+        
+        redirect('dashboard','refresh');
+      }else{
+        $this->session->set_flashdata('error', "User Yang Anda Masukkan Tidak/Belum Aktif");
+        redirect('login');
+      }
+      
+    }else{
+      $this->session->set_flashdata('error', "Email atau Password Salah");
+      redirect('login');
     }
+  }
+
+  function logout(){
+    $this->session->sess_destroy();
+    $url = base_url();
+    redirect($url);
+  }
 }
--- application/models/M_Auth.php
+++ application/models/M_Auth.php
@@ -7,4 +7,15 @@ class M_Auth extends CI_Model{
         $this->db->order_by('user.username', "ASC");
         return $this->db->get();
     }
+
+    function userLogin($username, $password){
+        $this->db->select('user.*, role.role');
+        $this->db->from('user');
+        $this->db->join('role', "user.idrole = role.id");
+        $this->db->where([
+            'user.username' => $username,
+            'user.password' => md5($password)
+        ]);
+        return $this->db->get();
+    }
 }
