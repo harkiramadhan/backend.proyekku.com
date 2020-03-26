637c253b8fcab6e3e4ceb8b5032fae7827cb4b8c
harkiramadhan <harkiramadhan@gmail.com>
2020-03-26 21:51:25 +0700

    update

--- application/views/admin_super/layout/header.php
+++ application/views/admin_super/layout/header.php
@@ -22,7 +22,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
-  <title>Proyekku.com</title>
+  <title>Proyekku.com | <?= $title ?></title>
   <!-- Favicon -->
   <link rel="icon" href="<?= base_url('') ?>/assets/img/brand/favicon.png" type="image/png">
   <!-- Fonts -->
