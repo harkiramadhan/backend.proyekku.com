699a006db300a9cf58c8713bc53a55621434bb10
harkiramadhan <harkiramadhan@gmail.com>
2020-04-25 01:26:59 +0700

    update

--- application/views/admin_pt/project.php
+++ application/views/admin_pt/project.php
@@ -169,7 +169,7 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="">Start <small class="text-warning"><strong>*</strong></small></label>
-                                        <div class='input-group date datetimepicker'>
+                                        <div class='input-group date datetimepicker' id="startDatetimepicker">
                                             <input name="start" type="text" class="form-control form-control-alternative form-control-sm" placeholder="Actual Start" onkeydown="return false" required>
                                             <span class="input-group-addon input-group-append">
                                                 <button class="btn btn-sm btn-primary ml-1" type="button" id="button-addon2">  <span class="fa fa-calendar"></span></button>
@@ -178,7 +178,7 @@
                                     </div>
                                     <div class="form-group">
                                         <label for="">End <small class="text-warning"><strong>*</strong></small></label>
-                                        <div class='input-group date datetimepicker'>
+                                        <div class='input-group date datetimepicker' id="endDatetimepicker">
                                             <input name="end" type="text" class="form-control form-control-alternative form-control-sm" placeholder="Actual End" onkeydown="return false" required>
                                             <span class="input-group-addon input-group-append">
                                                 <button class="btn btn-sm btn-primary ml-1" type="button" id="button-addon2">  <span class="fa fa-calendar"></span></button>
