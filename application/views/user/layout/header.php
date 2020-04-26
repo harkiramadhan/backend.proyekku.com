5b2a2d97966cd4124e220a95941260deaa9b9b77
harkiramadhan <harkiramadhan@gmail.com>
2020-04-26 16:12:33 +0700

    update

--- application/views/user/layout/header.php
+++ application/views/user/layout/header.php
@@ -301,7 +301,7 @@
                     <img alt="Image placeholder" src="<?= base_url('') ?>/assets/img/theme/team-4.jpg">
                   </span>
                   <div class="media-body  ml-2  d-none d-lg-block">
-                    <span class="mb-0 text-sm  font-weight-bold"><?= $username ?></span>
+                    <span class="mb-0 text-sm  font-weight-bold"><?= $username." - ".$role ?></span>
                   </div>
                 </div>
               </a>
